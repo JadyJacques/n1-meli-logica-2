3a61d0758eabbbc89e83c0ae8cc923c5ab4382fa
jadyjacques <jadyjacques.nave@gmail.com>
2019-09-23 22:24:19 -0300

    exercicios de array e objeto

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,26 @@
 //resposta aqui
+
+// var nomedavariavel = "jude";
+// console.log (nomedavariavel);
+
+// var listalegal = [ 'jude',
+// 'tem 17 anos',
+// 'seus produtos favoritos são: água, refrigerante, guaravita',
+// 'e é sua primeira vez comprando']
+// console.log (listalegal);
+
+var nome = 'jude';
+var idade = 15;
+var produtosfavoritos = ['água, refrigerante, guaravita'];
+var primeira = false;
+
+
+cliente = {
+    nome,
+    idade,
+    produtosfavoritos,
+    primeira
+}
+console.log(cliente);
+
+
